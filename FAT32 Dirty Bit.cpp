830bfec3bc5d1d7d23d8d40898c54f552d90cf25
DAVe3283 <DAVe3283@gmail.com>
2014-11-23 15:55:15 -0700

    Skeleton framework for validating sector.

--- FAT32 Dirty Bit.cpp
+++ FAT32 Dirty Bit.cpp
@@ -5,6 +5,8 @@
 
 int main(int argc, char* argv[])
 {
+	int driveId(3); // Use C: (3) for debug
+
 	// Get handle to VWIN32
 	HANDLE hDevice = CreateFile(
 		"\\\\.\\vwin32",
@@ -21,11 +23,16 @@ int main(int argc, char* argv[])
 	}
 
 	// Read a sector
-	char buffer[512] = { 0 };
-	BOOL result = NewReadSectors(hDevice, 3, 0, 1, (LPBYTE)buffer);
+	char buffer[SECTOR_SIZE] = { 0 };
+	BOOL success = NewReadSectors(
+		hDevice,
+		driveId,
+		0, // Start sector
+		1, // Sectors to read
+		(LPBYTE)buffer);
 
-	// Error?
-	if (result == FALSE)
+	// Returns TRUE on success
+	if (success == FALSE)
 	{
 		DWORD errNo = GetLastError();
 		printf("Unable to read raw sectors! Error code: 0x%08x\n", errNo);
@@ -37,7 +44,7 @@ int main(int argc, char* argv[])
 	printf("Got sector:\n\n");
 	printf("      00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
     printf("    /------------------------------------------------\n");
-	for (int i(0); i < (sizeof(buffer) / sizeof(*buffer)); ++i)
+	for (int i(0); i < SECTOR_SIZE; ++i)
 	{
 		// New line header
 		if (!(i % 0x10))
@@ -58,7 +65,19 @@ int main(int argc, char* argv[])
 			printf(" ");
 		}
 	}
-	printf("\nDone!\n");
+	printf("\n");
+
+	// Validate FAT 32 Volume ID (sanity check)
+	if (ValidFat32(buffer))
+	{
+		// DEBUG
+		printf("Valid FAT32 Volume ID!\n");
+	}
+	else
+	{
+		// DEBUG
+		printf("Not valid!\n");
+	}
 
 	// Done with raw disk I/O
 	CloseHandle(hDevice);
---
+++ Fat32Util.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+// Does the sector buffer contain a valid FAT32 Volume ID?
+bool ValidFat32(char sector[])
+{
+	// TODO: this
+	return false;
+}
+
+// Is the dirty bit set? (must be a valid FAT32 Volume ID!)
+bool GetVolumeDirty(void* sector)
+{
+	// TODO: this
+	return false;
+}
+
+// Set the dirty bit (must be a valid FAT32 Volume ID!)
+bool SetVolumeDirty(void* sectro)
+{
+	// TODO: this
+	return false;
+}
